87e2f9f66493bf75678bbbc15952a7f5bf4f066a
Aaron Parkening <aaron.parkening@gmail.com>
2019-04-18 15:22:22 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -4,10 +4,11 @@ def my_each(array)
     yield array[i]
     i += 1 
   end
+  array
 end
 
 
-collection = [1, 2, 3, 4]
-my_each(collection) do |i|
-  puts i
-end
+# collection = [1, 2, 3, 4]
+# my_each(collection) do |i|
+#   puts i
+# end
